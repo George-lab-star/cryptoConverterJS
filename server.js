4b80f30e71c36b048b2edcfcef28086c3f735696
George-lab-star <georgelomakin99@gmail.com>
2024-07-01 05:16:44 -0500

    added webserver

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, 'frontend')));
+app.use("/backend", express.static(path.join(__dirname, "backend")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`Сервер запущен на порту ${PORT}`);
+});
